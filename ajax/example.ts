9982960b2e4319adf2cdccacc34aa44e17963e46
jia wei <jw397@126.com>
2021-09-10 10:58:58 +0800

    feat(ajax): 样例

---
+++ ajax/example.ts
@@ -0,0 +1,36 @@
+import { BaseAjax } from "./mod.ts";
+
+class Ajax extends BaseAjax {
+  /**
+   * 处理消息，具体实现可以覆盖此项
+   */
+  protected handleMessage(msg: string) {
+    console.log("handleMessage", msg);
+    super.handleMessage(msg);
+  }
+
+  /**
+   * 处理错误请求
+   */
+  protected handleErrorResponse(response: Response) {
+    console.error(
+      `HTTP error, status = ${response.status}, statusText = ${response.statusText}`,
+    );
+  }
+}
+
+// Ajax.defaults.baseURL = "/api";
+
+export const ajax = new Ajax();
+
+interface User {
+  name: string;
+}
+
+for (let i = 0; i < 100; i++) {
+  ajax.get<User>("http://localhost:4000/api", {
+    1: 1,
+  }, {
+    timeout: 100,
+  }).then((res) => console.log(res));
+}
